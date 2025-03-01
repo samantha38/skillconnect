71851ad94e18e475c57cf27509a23a64dd99e567
samantha38 <samanthaanthony1512@gmail.com>
2025-03-01 20:54:25 +0530

    temp form

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import JobListings from "./components/JobListings";
+import JobForm from './components/JobForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [searchTitle, setSearchTitle] = useState("");
   const [searchLocation, setSearchLocation] = useState("");
+
+  const [refresh, setRefresh] = useState(false);
+
+    // Function to refresh job listings when a new job is added
+    const handleJobAdded = () => {
+        setRefresh(prev => !prev);
+    };
 
   return (
     <div>
@@ -13,6 +22,7 @@ const App = () => {
         setSearchTitle(title);
         setSearchLocation(location);
       }} />
+       <JobForm onJobAdded={handleJobAdded} />
       <JobListings searchTitle={searchTitle} searchLocation={searchLocation} />
     </div>
   );
